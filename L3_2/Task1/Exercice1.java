c9da9e6d9c096f53c3ecb8dcdd88af85088024d0
DzmitryParkhomenka <32838554+DzmitryParkhomenka@users.noreply.github.com>
2017-11-04 17:13:00 +0300

    Add files via upload

---
+++ L3_2/Task1/Exercice1.java
@@ -0,0 +1,35 @@
+package by.exercices.dzmitry.parkhomenka.les03_2.ex1;
+
+import java.util.Scanner;
+
+public class Exercice1 {
+
+	public static void main(String[] args) {
+		int k = 4;
+
+		Fraction[] mas = new Fraction[k];
+		inputFracion(mas, 4);
+		showFraction(mas);
+
+	}
+
+	public static void inputFracion(Fraction[] mas, int k) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < mas.length; i++) {
+
+			Fraction drob = new Fraction();
+			System.out.println("Enter numerator for " + i + " fraction");
+			drob.setM(sc.nextInt());
+			System.out.println("Enter denominator for " + i + " fraction");
+			drob.setN(sc.nextInt());
+
+			mas[i] = drob;
+		}
+	}
+
+	public static void showFraction(Fraction[] mas) {
+		for (int i = 0; i < mas.length; i++) {
+			System.out.print(mas[i].getM() + "/" + mas[i].getN() + " ");
+		}
+	}
+}
---
+++ L3_2/Task1/Fraction.java
@@ -0,0 +1,22 @@
+package by.exercices.dzmitry.parkhomenka.les03_2.ex1;
+
+public class Fraction {
+	int m;
+	int n;
+	
+	public void setM(int a) {
+		this.m = a;
+	}
+	
+	public int getM() {
+		return m;
+	}
+	
+	public void setN(int b) {
+		this.n = b;
+	}
+	
+	public int getN() {
+		return n;
+	}
+}
