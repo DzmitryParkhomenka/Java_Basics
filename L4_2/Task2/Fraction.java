e592e221fce3970686981c6599361a9b12fcffa8
DzmitryParkhomenka <32838554+DzmitryParkhomenka@users.noreply.github.com>
2017-11-21 13:12:11 +0300

    Add files via upload

---
+++ L4_2/Task2/Fraction.java
@@ -0,0 +1,8 @@
+package by.exercices.dzmitry.parkhomenka.les04_2.ex2;
+
+public class Fraction {
+
+	public static void main(String[] args) {
+		//	Решено в рамках Урока 3-2, задача 1
+	}
+}
---
+++ L4_2/Task3/Student.java
@@ -0,0 +1,8 @@
+package by.exercices.dzmitry.parkhomenka.les04_2.ex3;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// Решено в рамках Урока 3-2, задача 2
+	}
+}
